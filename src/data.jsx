4c1c07ec5057eed009e888e0ee37dd72665fdef6
Rohan Bhardwaj <113487993+rohanbhardwaj2005@users.noreply.github.com>
2024-08-07 10:08:35 +0000

    Update image path

--- src/data.jsx
+++ src/data.jsx
@@ -92,7 +92,7 @@ export const skills = [
 export const projects = [
   {
     id: nanoid(),
-    img: "public/cine-wishlist.png",
+    img: "/cine-wishlist.png",
     url: "https://cine-wishlist.netlify.app/",
     github: "https://github.com/rohanbhardwaj2005/",
     title: "Cine Wishlist",
@@ -108,7 +108,7 @@ export const projects = [
   },
   {
     id: nanoid(),
-    img: "public/recipe-rendezvous.png",
+    img: "/recipe-rendezvous.png",
     url: "https://recipe-rendezvous.vercel.app/",
     github: "https://github.com/rohanbhardwaj2005/",
     title: "Recipe Rendezvous",
